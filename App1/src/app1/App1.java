badecb346a41eefae5672fd91a4d724046956d24
system <system@AsusU45J>
2015-01-07 23:47:59 -0600

    For agregado

--- App1/src/app1/App1.java
+++ App1/src/app1/App1.java
@@ -15,7 +15,10 @@ public class App1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo y todos quienes lo habitan");
+        for (;;) {
+            System.out.println("Hola Mundo y todos quienes lo habitan");
+        }
+
     }
-    
+
 }
